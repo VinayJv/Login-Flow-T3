22ccc4245b0e3e1bc9b76c4ab34b8d69d68c9607
Vinay Jatav <vinay.jatav.dev@gmail.com>
2024-03-20 18:50:57 +0530

    Updated Variables

--- src/env.js
+++ src/env.js
@@ -17,9 +17,6 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-
-    USERNAME: z.string(),
-    PASSWORD: z.string(),
   },
 
   /**
@@ -38,8 +35,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    USERNAME: process.env.USERNAME,
-    PASSWORD: process.env.PASSWORD,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
